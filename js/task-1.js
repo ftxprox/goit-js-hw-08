2e0d4ca825101c22efbc32961fcd8b89df81ce5e
HryhorievOA <ftxprox@gmail.com>
2024-09-05 20:30:38 +0300

    hw 08

--- js/task-1.js
+++
@@ -1,6 +0,0 @@
-const categories = document.querySelectorAll("ul#categories>li.item");
-    console.log(`Number of categories: ${categories.length}`)
-categories.forEach(element => {
-    console.log(`Category: ${element.querySelector("h2").textContent}`);
-    console.log(`Elements: ${element.querySelectorAll("ul>li").length}`);
-});
--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputName = document.querySelector("input#name-input");
-const outputName = document.querySelector("span#name-output");
-inputName.addEventListener("input", (event) => {
-    const eventValue = String(event.currentTarget.value).trim();
-    if (eventValue != false) outputName.textContent = eventValue;
-    else if (!eventValue) outputName.textContent = "Anonymous";
-});
